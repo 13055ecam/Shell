9df302edf9d0b318cc69414d7a2a43217c3ad7a3
Alexandre Lenaerts <13055@ecam.be>
2017-12-14 23:53:02 +0100

    Add files via upload

--- stats.c
+++ stats.c
@@ -4,11 +4,12 @@
 #include <inttypes.h>
 #include <wchar.h>
 #include "stats.h"
+#include <time.h>
 #include <sys/types.h>
 #include <dirent.h>
 #include <sys/stat.h>
 #include <time.h>
-
+#include <unistd.h>
 
 
 void stats(const char *directory)
@@ -19,6 +20,7 @@ void stats(const char *directory)
     int pos=0; 
     
     struct stat st;
+    struct stat bt;
     char name_header[15] = "nom";
     char date_header[15] = "date";
     char size_header[15] = "taille";
@@ -26,8 +28,8 @@ void stats(const char *directory)
     /* Resetting files counter */
     int total_files = 0; 
     
-    float  total_size = 0  
-    float size = 0;
+    float  total_size = 0;
+    float size;
     
     /*Header tab : name - Date of latest modifications - files size */
     printf ("\n");
@@ -36,6 +38,7 @@ void stats(const char *directory)
     printf ("\n");
     
     /* Exploring a directory and returning number of files from current directory */
+    int rep = chdir(directory);
     int n = scandir(directory, &namelist,NULL,alphasort);
     
     /* Returning -1 in case of error */
@@ -48,33 +51,34 @@ void stats(const char *directory)
         while (pos<n)
         {
             stat (namelist[pos]->d_name, &st);
-            time_t t = st.st_atime;
+            time_t t = st.st_mtime;
             struct tm tm = *localtime (&t);
             char date[32]; /* creating a format date */
             char name[32];
-            strftime (date, sizeof date, "%d/%m/%Y %H:%M:%S", &tm);
+            strftime(date, sizeof date, "%d/%m/%Y %H:%M:%S", &tm);
+
             float size = st.st_size;
             
              /*Not showing  "." , ".." and ".DC_Store" files in the tab*/
-            if (strcmp(namelist[i]->d_name, ".") != 0 && strcmp(namelist[i]->d_name, "..") != 0 && strcmp(namelist[i]->d_name, ".DS_Store")!=0 )
+            if (strcmp(namelist[pos]->d_name, ".") != 0 && strcmp(namelist[pos]->d_name, "..") != 0 && strcmp(namelist[pos]->d_name, ".DS_Store")!=0 )
             {
-                if (strlen(namelist[i]->d_name)>10)
+                if (strlen(namelist[pos]->d_name)>10)
                 {
-                if (r > 1024*1024*1024 && size< 1099511627776)
+                if (size > 1024*1024*1024 && size< 1099511627776)
                 {
-                    printf ("%25s |%10s| %1.2f G\n",strncpy(name,namelist[i]->d_name,20),date,size/(1024*1024*1024));
+                    printf ("%25s |%10s| %1.2f G\n",strncpy(name,namelist[pos]->d_name,20),date,size/(1024*1024*1024));
                 }
-                if (r > 1024*1024 && r < 1024*1024*1024)
+                if (size > 1024*1024 && size < 1024*1024*1024)
                 {
-                    printf ("%25s |%10s| %1.2f M\n",strncpy(name,namelist[i]->d_name,20),date,size/(1024*1024));
+                    printf ("%25s |%10s| %1.2f M\n",strncpy(name,namelist[pos]->d_name,20),date,size/(1024*1024));
                 }
-                if (r > 1024 && r < 1024*1024)
+                if (size> 1024 && size< 1024*1024)
                 {
-                    printf ("%25s |%10s| %1.2f K\n",strncpy(name,namelist[i]->d_name,20),date,size/1024);
+                    printf ("%25s |%10s| %1.2f K\n",strncpy(name,namelist[pos]->d_name,20),date,size/1024);
                 }
-                if (r < 1024)
+                if (size < 1024)
                 {
-                    printf ("%25s |%10s| %1.2f B\n",strncpy(name,namelist[i]->d_name,20),date,size);
+                    printf ("%25s |%10s| %1.2f B\n",strncpy(name,namelist[pos]->d_name,20),date,size);
                 }
                 }
                 total_files++;
