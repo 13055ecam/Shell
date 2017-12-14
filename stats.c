b653a40c769aa97914f6868bba5ccdc4a46a5f3a
Alexandre Lenaerts <13055@ecam.be>
2017-12-14 21:03:38 +0100

    Update stats.c

--- stats.c
+++ stats.c
@@ -10,114 +10,105 @@
 #include <time.h>
 
 
-/* Ne compte pas la taille des dossiers mais seulement des fichiers dans les dossiers !*/
-
 
 void stats(const char *directory)
 {
     struct dirent **namelist;
-    int n;
-    int i=0;
+    
+    /* set the position of file in a list */  
+    int pos=0; 
+    
     struct stat st;
-    char nom[15] = "nom";
-    char date[15] = "date";
-    char taille[15] = "taille";
-    int nbr = 0; /* nombre de fichier*/
-    float  v=0,r,ptr;
+    char name_header[15] = "nom";
+    char date_header[15] = "date";
+    char size_header[15] = "taille";
+    
+    /* Resetting files counter */
+    int total_files = 0; 
+    
+    float  total_size = 0  
+    float size = 0;
+    
+    /*Header tab : name - Date of latest modifications - files size */
     printf ("\n");
-    printf (" |%s                     |%s               |%s\n",nom,date,taille);
+    printf (" |%s                     |%s               |%s\n",name_header,date_header,size_header);
     printf ("-|------------------------|-------------------|------------------");
     printf ("\n");
-    n = scandir(directory, &namelist,NULL,alphasort);
+    
+    /* Exploring a directory and returning number of files from current directory */
+    int n = scandir(directory, &namelist,NULL,alphasort);
+    
+    /* Returning -1 in case of error */
     if (n < 0)
     {
         perror("scandir");
     }
     else 
     {
-        while (i<n)
+        while (pos<n)
         {
-            stat (namelist[i]->d_name, &st);
+            stat (namelist[pos]->d_name, &st);
             time_t t = st.st_atime;
             struct tm tm = *localtime (&t);
-            char s[32];
+            char date[32]; /* creating a format date */
             char name[32];
-            strftime (s, sizeof s, "%d/%m/%Y %H:%M:%S", &tm);
-            float r = st.st_size;
-            /**
-             int err;
-             regex_t preg;
-             const char *str_request = namelist[i]->d_name;
-             const char *str_regex = ".*\\.pdf";
-             err = regcomp (&preg, str_regex, REG_NOSUB | REG_EXTENDED);
-             if (err == 0)
-             {
-             int match;
-             match = regexec (&preg, str_request, 0, NULL, 0);
-             regfree (&preg);
-             if (match == 0)
-             {
-             printf ("type pdf");
-             }
-             else
-             {
-             printf ("Inconnu");
-             }
-             }
-             **/ 
+            strftime (date, sizeof date, "%d/%m/%Y %H:%M:%S", &tm);
+            float size = st.st_size;
             
+             /*Not showing  "." , ".." and ".DC_Store" files in the tab*/
             if (strcmp(namelist[i]->d_name, ".") != 0 && strcmp(namelist[i]->d_name, "..") != 0 && strcmp(namelist[i]->d_name, ".DS_Store")!=0 )
             {
                 if (strlen(namelist[i]->d_name)>10)
                 {
-                /* nom - date de la dernière modif - taille du fichier*/
-                if (r > 1024*1024*1024 && r < 1099511627776)
+                if (r > 1024*1024*1024 && size< 1099511627776)
                 {
-                    printf ("%25s |%10s| %1.2f G\n",strncpy(name,namelist[i]->d_name,20),s,r/(1024*1024*1024));
+                    printf ("%25s |%10s| %1.2f G\n",strncpy(name,namelist[i]->d_name,20),date,size/(1024*1024*1024));
                 }
                 if (r > 1024*1024 && r < 1024*1024*1024)
                 {
-                    printf ("%25s |%10s| %1.2f M\n",strncpy(name,namelist[i]->d_name,20),s,r/(1024*1024));
+                    printf ("%25s |%10s| %1.2f M\n",strncpy(name,namelist[i]->d_name,20),date,size/(1024*1024));
                 }
                 if (r > 1024 && r < 1024*1024)
                 {
-                    printf ("%25s |%10s| %1.2f K\n",strncpy(name,namelist[i]->d_name,20),s,r/1024);
+                    printf ("%25s |%10s| %1.2f K\n",strncpy(name,namelist[i]->d_name,20),date,size/1024);
                 }
                 if (r < 1024)
                 {
-                    printf ("%25s |%10s| %1.2f B\n",strncpy(name,namelist[i]->d_name,20),s,r);
+                    printf ("%25s |%10s| %1.2f B\n",strncpy(name,namelist[i]->d_name,20),date,size);
                 }
                 }
-                nbr++;
-                v+=r;
+                total_files++;
+                total_size+=size;
             }
-            ++i;
+            ++pos;
         }
+        
+        /* footer tab */
         printf ("-|------------------------|-------------------|------------------");
         printf ("\n");
+        
+        /*Printing a summary of the directory */
         printf ("Résumé\n");
         printf (" ------\n");
         printf ("\n");
-        
-        /* taille totale du dossier */
-        
-        if (v > 1024*1024*1024 && v < 1099511627776)
+      
+        if (total_size > 1024*1024*1024 && total_size < 1099511627776)
         {
-            printf ("Taille totale du dossier : %1.2f G\n",v/(1024*1024*1024));
+            printf ("Taille totale du dossier : %1.2f G\n",total_size/(1024*1024*1024));
         }
-        if (v > 1024*1024 && v < 1024*1024*1024)
+        if (total_size > 1024*1024 && total_size < 1024*1024*1024)
         {
-            printf ("Taille totale du dossier : %1.2f M\n",v/(1024*1024));
+            printf ("Taille totale du dossier : %1.2f M\n",total_size/(1024*1024));
         }
-        if (v > 1024 && v < 1024*1024)
+        if (total_size > 1024 && total_size < 1024*1024)
         {
-            printf ("Taille totale du dossier : %1.2f K\n",v/1024);
+            printf ("Taille totale du dossier : %1.2f K\n",total_size/1024);
         }
-        if (v < 1024)
+        if (total_size < 1024)
         {
-            printf ("Taille totale du dossier : %1.2f B\n",v);
+            printf ("Taille totale du dossier : %1.2f B\n",total_size);
         }
-        printf ("Nombre total de fichiers : %d\n",nbr);
+        printf ("Nombre total de fichiers : %d\n",total_files);
     }
 }
 
