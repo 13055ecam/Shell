fc1e95260929d15522205a0648ac584fa3cf7e2f
Alexandre Lenaerts <13055@ecam.be>
2017-12-16 16:24:11 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -6,20 +6,56 @@
 #include "stats.h"
 #include "clnup.h"
 #include "shrink.h"
+#include <unistd.h>
+#include <getopt.h>
+
+
 
 /* terminal*/
-int main(void)
+int main(int argc, char* argv[] )
 {
     char test[15];
     char str[BUFSIZ];
-    printf("Enter a path :");
-    scanf("%s", str);
-    while (1) {
+    int option = 0;
+    int h = -1;
+    int r = -1;
+   
+    while(1){
+      
+        if ((option = getopt(argc, argv,"rh:h:")) != -1) {
+        switch (option) {
+            case 'h':
+                if (!strcmp("stats", optarg)){
+                        printf("Write <stats> to show all informations about this folder\n");
+                }
+                else if (!strcmp("shrink", optarg)){
+                        printf("Write <shrink> to compress several files of this folder\n");
+                }
+                else if (!strcmp("clnup", optarg)){
+                        printf("Write <clnup> to sort files of this folder in multiple folders according the type file \n");
+                }
+                else if (!strcmp("sort", optarg)){
+                        printf("Write <sort> \n");
+                }
+                exit(EXIT_FAILURE);
+                break;
+            exit(EXIT_FAILURE);
+            case 'r' : r =0;
+                printf("Enter a path :");
+                scanf("%s", str);
+                break;
+            default:
+            exit(EXIT_FAILURE);
+                break;
+        }
+    }
+        // Help the user
+
+        else if (r==0){
         /* Directory exists. */
         DIR* dir = opendir(str);
         if (dir) {
             scanf("%s", test);
-
             if (!strcmp("shrink", test)) {
                 char min_size[50];
                 printf("Enter the minimal compressible size :");
@@ -41,6 +77,8 @@ int main(void)
             DIR* dir = opendir(str);
             scanf("%s", str);
         }
+        }
     }
 }
+
 
