470989b2d43348f3b3d266584432a910a4e16614
Alexandre Lenaerts <13055@ecam.be>
2017-12-16 22:22:36 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -16,74 +16,87 @@ int main(int argc, char* argv[] )
 {
     char test[15];
     char str[BUFSIZ];
-    int option = 0;
     int h = -1;
     int r = -1;
    
     while(1){
-      
-        if ((option = getopt(argc, argv,"rh:h:")) != -1) {
-        switch (option) {
+        
+        int option = getopt(argc, argv,"rh:hr:");
+        switch (option)
+        
+        {
+            /* Help commandes */
             case 'h':
-                if (!strcmp("stats", optarg)){
-                        printf("Write <stats> to show all informations about this folder\n");
+                if (!strcmp("stats", optarg))
+                {
+                    printf("Write <stats> to show all informations about this folder\n");
                 }
-                else if (!strcmp("shrink", optarg)){
-                        printf("Write <shrink> to compress several files of this folder\n");
+                else if (!strcmp("shrink", optarg))
+                {
+                    printf("Write <shrink> to compress several files of this folder\n");
                 }
-                else if (!strcmp("clnup", optarg)){
-                        printf("Write <clnup> to sort files of this folder in multiple folders according the type file \n");
+                else if (!strcmp("clnup", optarg))
+                {
+                    printf("Write <clnup> to sort files of this folder in multiple folders according the type file \n");
                 }
                 else if (!strcmp("sortn", optarg)){
                         printf("Write <sortn> \n");
                 }
                 exit(EXIT_FAILURE);
                 break;
-            exit(EXIT_FAILURE);
-            case 'r' : r =0;
-                printf("Enter a path :");
-                scanf("%s", str);
+                
+            /* run commandes */
+            case 'r' :
+                r =0;
                 break;
             default:
             exit(EXIT_FAILURE);
                 break;
         }
-    }
-        // Help the user
-
-        else if (r==0){
-        /* Directory exists. */
-        DIR* dir = opendir(str);
-        if (dir) {
-            printf("Launch a command :");
-            scanf("%s", test);
-            if (!strcmp("shrink", test)) {
-                char min_size[50];
-                printf("Enter the minimal compressible size :");
-                scanf("%s", min_size);
-                shrink(atoi(min_size));
-            }
-            else if(!strcmp("sortn", test)) {
-                //sortn();
-            }
-            else if(!strcmp("stats", test)) {
-                stats(str);
-            }
-            else if(!strcmp("clnup", test)) {
-                clnup(str);
-            }
-            else if(!strcmp("quit", test) ) {
-                exit(0);
-            }
-            else{
-                printf("This command doesn't exist ! Try with stats, shrink, clnup or sortn.More infos in readme.m file!\n");
-            }
-        } else if (ENOENT == errno) {
-            // Directory not exists.
-            printf("Enter a correct path :");
-            DIR* dir = opendir(str);
+        
+        if (r==0)
+        {
+            printf("Enter a path :");
             scanf("%s", str);
-        }
+            
+            /* Directory exists. */
+            DIR* dir = opendir(str);
+            
+            if (dir)
+            {
+                printf("Launch a command :");
+                scanf("%s", test);
+                
+                if (!strcmp("shrink", test)) {
+                    char min_size[50];
+                    printf("Enter the minimal compressible size :");
+                    scanf("%s", min_size);
+                    shrink(atoi(min_size));
+                }
+                else if(!strcmp("sortn", test)) {
+                    //sortn();
+                }
+                else if(!strcmp("stats", test)) {
+                    stats(str);
+                }
+                else if(!strcmp("clnup", test)) {
+                    clnup(str);
+                }
+                else if(!strcmp("quit", test) ) {
+                    exit(0);
+                }
+                else{
+                    printf("This command doesn't exist ! Try with stats, shrink, clnup or sortn.More infos in readme.m file!\n");
+                }
+            }
+            
+            // Directory doesn't exist.
+            else if (ENOENT == errno)
+            {
+                printf("Enter a correct path :");
+                DIR* dir = opendir(str);
+                scanf("%s", str);
+            }
         }
     }
 }
