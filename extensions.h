12d40eefd0b0c7f40b8d4b07fe14418248a3dca3
Gaétan Guru <gaetan.guru@gmail.com>
2017-12-20 11:02:40 +0100

    Extension modification

--- extensions.h
+++ extensions.h
@@ -1,39 +1,39 @@
 char *extension(char *type){
   char *buffer="Unknown";
-  if(strncmp("pdf",type,10)==0){
+  if(strncasecmp("pdf",type,10)==0){
     buffer = "Pdf files";
   }
-  else if(strncmp("cmd",type,10)==0){
+  else if(strncasecmp("cmd",type,10)==0){
     buffer = "Command files";
   }
-  else if(strncmp("jpg",type,10)==0 || strncmp("png",type,10)==0){
+  else if(strncasecmp("jpg",type,10)==0 || strncasecmp("png",type,10)==0){
     buffer = "Image";
   }
-  else if(strncmp("exe",type,10)==0){
+  else if(strncasecmp("exe",type,10)==0){
     buffer = "Executable windows";
   }
-  else if(strncmp("pptx",type,10)==0){
+  else if(strncasecmp("pptx",type,10)==0){
     buffer = "Powerpoint";
   }
-  else if(strncmp("xlsx",type,10)==0){
+  else if(strncasecmp("xlsx",type,10)==0){
     buffer = "Excel files";
   }
-  else if(strncmp("ics",type,10)==0){
+  else if(strncasecmp("ics",type,10)==0){
     buffer = "Timetable";
   }
-  else if(strncmp("mp3",type,10)==0 || strncmp("wav",type,10)==0 || strncmp("flac",type,10)==0 || strncmp("aac",type,10)==0){
+  else if(strncasecmp("mp3",type,10)==0 || strncasecmp("wav",type,10)==0 || strncasecmp("flac",type,10)==0 || strncasecmp("aac",type,10)==0){
     buffer = "Audio files";
   }
-  else if(strncmp("mkv",type,10)==0 || strncmp("mp4",type,10)==0){
+  else if(strncasecmp("mkv",type,10)==0 || strncasecmp("mp4",type,10)==0){
     buffer = "Movies";
   }
-  else if(strncmp("torrent",type,10)==0){
+  else if(strncasecmp("torrent",type,10)==0){
     buffer = "Torrent files";
   }
-  else if(strncmp("zip",type,10)==0 || strncmp("gz",type,10)==0){
+  else if(strncasecmp("zip",type,10)==0 || strncasecmp("gz",type,10)==0){
     buffer = "Zip files";
   }
-  else if(strncmp("docx",type,10)==0){
+  else if(strncasecmp("docx",type,10)==0){
     buffer = "Word files"
   }
   return buffer;
